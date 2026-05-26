0d559845eed2541fc325ca02f44074eaead4ec94
PatilHarsh97 <patilharshmaruti1710@gmail.com>
2026-05-26 12:00:22 +0530

    Add greeter module with greet and farewell functions

---
+++ greeter.js
@@ -0,0 +1,9 @@
+function greet(name) {
+    return `Hello, ${name}! Welcome to LucidGrasp.`;
+}
+
+function farewell(name) {
+    return `Goodbye, ${name}. See you next time!`;
+}
+
+module.exports = { greet, farewell };
