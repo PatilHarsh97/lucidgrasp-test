e52756bfd01d5bf586968b4774294536c506e702
PatilHarsh97 <patilharshmaruti1710@gmail.com>
2026-05-26 12:19:58 +0530

    Delete greeter.js

--- greeter.js
+++
@@ -1,9 +0,0 @@
-function greet(name) {
-    return `Hello, ${name}! Welcome to LucidGrasp.`;
-}
-
-function farewell(name) {
-    return `Goodbye, ${name}. See you next time!`;
-}
-
-module.exports = { greet, farewell };
